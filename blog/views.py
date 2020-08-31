0e6ea4bf1746b68f05612f489d4c752aeed34508
Idadel <idadelm@gmail.com>
2020-08-31 04:45:39 +0100

    imported Post database to views and registered admin

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Ida',
-        'title': 'Blog Post 1',
-        'content': 'First post created',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Delphine',
-        'title': 'Blog Post 2',
-        'content': 'Second post created',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
+from .models import Post
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all
     }
     return render(request, 'blog/home.html', context)
 
