a5a97ac869238f7b06f0274696fe07d7e682a616
Idadel <idadelm@gmail.com>
2020-08-31 03:25:42 +0100

    added base template and static css

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Ida',
+        'title': 'Blog Post 1',
+        'content': 'First post created',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Delphine',
+        'title': 'Blog Post 2',
+        'content': 'Second post created',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'about/home.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
